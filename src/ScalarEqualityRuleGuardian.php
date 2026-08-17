514f038f463b366a094e435e4d81477b4845b724
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-08-17 10:59:25 +0400

    [AEGISORA-68167481] Implemented $guardian attribute.

--- src/ScalarEqualityRuleGuardian.php
+++ src/ScalarEqualityRuleGuardian.php
@@ -2,6 +2,15 @@
 
 namespace Aegisora\RuleGuardians\ScalarEqualityRule;
 
+use Aegisora\Guardian\Guardian;
+
 class ScalarEqualityRuleGuardian
 {
+    private Guardian $guardian;
+
+    public function __construct(
+        Guardian $guardian
+    ) {
+        $this->guardian = $guardian;
+    }
 }
